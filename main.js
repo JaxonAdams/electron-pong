9388b883fb89711ed93b36689a750a5c2ac58f42
Jaxon Adams <jadamsresume1@gmail.com>
2022-10-06 18:46:36 -0600

    linked stylesheet and set custom menu

--- main.js
+++ main.js
@@ -2,24 +2,39 @@
 
 // import modules
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // create app window
 const createWindow = () => {
 	const window = new BrowserWindow({
 		width: 800,
-		height: 600
+		height: 600,
+		resizable: false
 	});
 
 	window.loadFile(path.join(__dirname, './views/index.html'));
 };
+
+// menu template
+const menuTemplate = [
+	{
+		label: 'Options',
+		submenu: [
+			{ role: 'quit' }
+		]
+	}
+];
+
+// set up menu from template
+const menu = Menu.buildFromTemplate(menuTemplate);
+Menu.setApplicationMenu(menu);
 
 // when app is ready, open window
 app.whenReady().then(() => {
 	createWindow();
 
 	// open new window if no windows are open
-	app.on('activate' () => {
+	app.on('activate', () => {
 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 	});
 });
