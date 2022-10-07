469b706601eada6f4fed639f0ca841ff21b9c65f
Jaxon Adams <jadamsresume1@gmail.com>
2022-10-06 19:05:44 -0600

    began canvas setup

---
+++ views/assets/script.js
@@ -0,0 +1,8 @@
+// get canvas element
+const canvas = document.querySelector('#game-canvas');
+
+const ctx = canvas.getContext('2d');
+
+// set canvas width and height
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
